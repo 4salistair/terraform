5d61661f02e0c9f2520164e4998c78adc89e32d3
4salistair <4salistair@gmail.com>
2020-04-08 07:54:49 +0100

    more comments

--- example.tf
+++ example.tf
@@ -1,3 +1,14 @@
+    ///////// ****     TERRAFROM FIRST EXAMPLE    **** ////////
+
+// Example terraform file that creates resource on AWS
+// there is a local AWS api key (refrenced in profile    = "default")
+// in ~/.aws/credentials - 
+
+// Used three simple commands 
+// terraform init - initalises the current folder - looks for files with .tf extension 
+// terraform apply - exicutes any .tf files held in the init folder, there for creates resourse
+// terraform destroy - rips down the infrastucture created using apply
+
 provider "aws" {
                 // provider is the cloud platform
     profile    = "default"
